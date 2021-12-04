aeabdf9a9db660af02adaaf42111d1d584dd5299
Jessica <jessmichaud488@gmail.com>
2021-12-04 18:11:34 -0500

    updated types

--- Chopkins/Chopkins/Form1.vb
+++ Chopkins/Chopkins/Form1.vb
@@ -1,36 +1,34 @@
 ﻿Public Class Form1
     Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
-        Dim twelvePack, fivePack, twoPack, totalPack,
-            firstTotal, secondTotal, ThirdTotal,
-            totalShipping, shippingCharge As Double
+        Dim dblTwelvePack, dblFivePack, dblTwoPack, dblTotalPack,
+        dblFirstTotal, dblSecondTotal, dblThirdTotal,
+        dblTotalShipping, dblShippingCharge As Double
 
-        Double.TryParse(txtTwelveBox.Text, twelvePack)
-        firstTotal = twelvePack * 14.99
+        Double.TryParse(txtTwelveBox.Text, dblTwelvePack)
+        dblFirstTotal = dblTwelvePack * 14.99
 
-        lblTwelveTotalBox.Text = FormatCurrency(firstTotal)
+        lblTwelveTotalBox.Text = FormatCurrency(dblFirstTotal)
 
-        Double.TryParse(txtFiveBox.Text, fivePack)
-        secondTotal = fivePack * 6.99
+        Double.TryParse(txtFiveBox.Text, dblFivePack)
+        dblSecondTotal = dblFivePack * 6.99
 
-        lblFiveTotalBox.Text = FormatCurrency(secondTotal)
+        lblFiveTotalBox.Text = FormatCurrency(dblSecondTotal)
 
-        Double.TryParse(txtTwoBox.Text, twoPack)
-        ThirdTotal = twoPack * 2.5
+        Double.TryParse(txtTwoBox.Text, dblTwoPack)
+        dblThirdTotal = dblTwoPack * 2.5
 
-        lblTwoTotalBox.Text = FormatCurrency(ThirdTotal)
+        lblTwoTotalBox.Text = FormatCurrency(dblThirdTotal)
 
-        totalPack = twelvePack + fivePack + twoPack
-        lblQtyBox.Text = totalPack
+        dblTotalPack = dblTwelvePack + dblFivePack + dblTwoPack
+        lblQtyBox.Text = dblTotalPack
 
-        Double.TryParse(txtShippingBox.Text, shippingCharge)
-        totalShipping = firstTotal + secondTotal + ThirdTotal + shippingCharge
-        lblTotalBox.Text = FormatCurrency(totalShipping)
+        Double.TryParse(txtShippingBox.Text, dblShippingCharge)
+        dblTotalShipping = dblFirstTotal + dblSecondTotal + dblThirdTotal + dblShippingCharge
+        lblTotalBox.Text = FormatCurrency(dblTotalShipping)
     End Sub
     Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
         Me.Close()
     End Sub
-
-    Private Sub txtFiveBox_TextChanged(sender As Object, e As EventArgs)
 
     End Sub
 End Class
